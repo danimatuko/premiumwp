c1b11947337e9d1018c9ec79ec83dd853c6c05f2
Dani Matuko <matuko305@gmail.com>
2023-03-01 16:31:12 +0200

    escape data in helpers

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,9 +1,9 @@
 <?php
 
 function firsttheme_post_meta() {
-    $author_url = get_author_posts_url(get_the_author_meta('ID'));
-    $author_name = get_the_author();
-    $post_date =  get_the_date();
+    $author_url = esc_attr(get_author_posts_url(get_the_author_meta('ID')));
+    $author_name = esc_html(get_the_author());
+    $post_date =  esc_attr(get_the_date());
 
     echo
     <<<POSTMETA
