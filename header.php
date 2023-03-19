cb4e0a007cd301c7778cd78375309979b35a3862
Dani Matuko <matuko305@gmail.com>
2023-03-19 12:24:43 +0200

    add logo theme support

--- header.php
+++ header.php
@@ -13,14 +13,16 @@
         <nav class="navbar py-5 navbar-expand-lg navbar-dark bg-dark">
 
             <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a class="navbar-brand text-primary"
-                        href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+
+                    <?php if (has_custom_logo()) {
+                        the_custom_logo();
+                    } ?>
+
+                    <a class="navbar-brand text-primary" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
                     <?php
                     wp_nav_menu(array(
                         'theme_location' => 'main-menu',
@@ -29,7 +31,7 @@
                         'fallback_cb' => '__return_false',
                         'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
                         'depth' => 2,
-                        'walker' => new bootstrap_5_wp_nav_menu_walker()
+                        'walker' => new bootstrap_5_wp_nav_menu_walker(),
                     ));
                     ?>
 
--- lib/theme-support.php
+++ lib/theme-support.php
@@ -2,6 +2,12 @@
 function _themename_theme_support() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo', array(
+        'height' => 64,
+        'width' => 64,
+        // 'flex-height' => true,
+        // 'flex-width' => true,
+    ));
     add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script'));
 }
 
